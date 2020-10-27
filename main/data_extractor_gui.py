836b7355c330b4af2ce45f6a6523436c7a877c3a
J Devlin <jwd.devlin@gmail.com>
2020-10-27 21:20:49 +0100

    Create window frames (#3)
    * Setup individual frames for separate operation areas.
    
    * Setup individual frames for separate operation areas.

--- main/data_extractor_gui.py
+++ main/data_extractor_gui.py
@@ -10,16 +10,18 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.frame = tk.Frame(master)
+
         # Initial Image
         self.__default_image_file = '../resources/default_graph.png'
 
+        # Set up Frames
+        self._initial_setup_frames()
+
         # Reference for Objects:
-        self.x = self.y = 0
         self.bounding_boxes = {}
 
         # Set canvas fo images
-        self.canvas = tk.Canvas(self.master, width=1280, height=800, cursor="cross")
+        self.canvas = tk.Canvas(self.frame_canvas, width=1280, height=800, cursor="cross")
         self.canvas.pack()
 
         # Load Default Images
@@ -27,9 +29,16 @@ class Application(tk.Frame):
 
 
         # Button for Switching the Image
-        self.button_switch_image = tk.Button(self.frame, text="Switch Image", command=self.update_setup_for_new_image)
-        self.button_switch_image.pack(side="right")
-        self.frame.pack()
+        self.button_switch_image = tk.Button(self.frame_buttons_static, text="Switch Image", command=self.update_setup_for_new_image)
+        self.button_switch_image.pack(side=tk.LEFT)
+
+    def _initial_setup_frames(self):
+        self.frame_canvas = tk.Frame(self.master, bg="yellow")
+        self.frame_buttons_static = tk.Frame(self.master, bg="blue")
+        self.frame_input_info = tk.Frame(self.master, bg="green")
+        self.frame_canvas.grid(row=0, column=0, rowspan=4, columnspan=8, sticky=(tk.N, tk.S, tk.W, tk.E))
+        self.frame_buttons_static.grid(row=0, column=8, rowspan=1, columnspan=2, sticky=(tk.N, tk.S, tk.W, tk.E))
+        self.frame_input_info.grid(row=1, column=8, rowspan=3, columnspan=2, sticky=(tk.N, tk.S, tk.W, tk.E))
 
     def _initial_setup_image(self):
         # Draw Default Image
@@ -40,24 +49,24 @@ class Application(tk.Frame):
         self.image_widget = self.canvas.create_image(0, 0, anchor="nw", image=self.display_image)
         # Add bounding box elemnt
         self.bounding_box_creator = self.initialise_bounding_box_creator()
-        self.button_bounding_box = tk.Button(self.frame, text="Bounding Box",
+        self.button_bounding_box = tk.Button(self.frame_buttons_static, text="Bounding Box",
                                              command=self.bounding_box_creator.bind_bounding_box_creation)
-        self.button_bounding_box.pack(side="right")
+        self.button_bounding_box.pack(side=tk.LEFT)
 
     def load_image(self, filename: str):
         self.display_image = ImageTk.PhotoImage(Image.open(filename))
 
-
-    def openfn(self):
+    @staticmethod
+    def __select_file_name():
         return filedialog.askopenfilename(title='open')
 
     def initialise_bounding_box_creator(self):
         # Refresh bounding boxes
         self.bounding_boxes = {}
-        return BoundingBoxElements(self.canvas, self.bounding_boxes, self.processing_image, self.master)
+        return BoundingBoxElements(self.canvas, self.bounding_boxes, self.processing_image, self.frame_input_info)
 
-    def update_setup_for_new_image(self, *args):
-        file_name = self.openfn()
+    def update_setup_for_new_image(self):
+        file_name = self.__select_file_name()
         if file_name:
             # Clear Bounding Boxes
             self.bounding_box_creator.clear_all_bounding_box_elements()
